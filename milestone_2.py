50447ad35d423f79ffde9daa6cd5a06a773f7feb
whistlecic <27658625+whistlecic@users.noreply.github.com>
2024-09-24 16:38:17 +0100

    Add user input and validation

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,21 @@
+import random
 # This is a comment
 '''This is a Docstring'''
 
 word_list = ['apple', 'watermelon', 'cherry', 'peach', 'blueberry']
 
-print(word_list)    
+print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = str(input("Enter a single letter."))
+
+def check_valid_input():
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+check_valid_input()
