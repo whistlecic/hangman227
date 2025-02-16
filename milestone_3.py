512655d61cac056ec2abee786531dbac1d4b338f
Cicely Henderson <cicely@JBJS.LOCAL>
2025-02-16 14:23:39 +0000

    Add milestone_5 file

--- milestone_3.py
+++ milestone_3.py
@@ -9,20 +9,21 @@ word = random.choice(word_list)
 
 def ask_for_input():
     #Function to get user's guess for a letter in the word.
-    guess = str(input("Enter a letter: "))
-    check_guess(guess)
-    while True:
-        if guess in word:
-            print(f"Good guess! {guess} is in the word.")
-        else:
-            print(f"Sorry, {guess} is not in the word. Please try again.")
-        break
+    guess = str(input("Enter a letter: ")).lower().strip()
+    
+    if not check_guess(guess):  # Checks whether guess is valid
+        return ask_for_input()  # If invalid asks for input again
+
+    if guess in word:
+        print(f"Good guess! '{guess}' is in the word.")
+    else:
+        print(f"Sorry, '{guess}' is not in the word. Please try again.")
 
 def check_guess(guess):
     if len(guess) == 1 and guess.isalpha():
         return True
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print("Invalid letter. Please,  enter a single alphabetical character.")
         return False
 
 ask_for_input()
